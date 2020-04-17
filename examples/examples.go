68f45991c2676ef291e02c9f8dd193967df83d17
Thibaut Ackermann <thibaut@ackermann.dev>
2020-04-17 20:40:34 +0200

    Add example.go empty file to fix (?) travis build issue

---
+++ examples/examples.go
@@ -0,0 +1,4 @@
+package examples
+
+// Empty file to fix travis build issue
+// go build github.com/thib-ack/rehapt/examples: no buildable Go source files in /home/travis/gopath/src/github.com/thib-ack/rehapt/examples
